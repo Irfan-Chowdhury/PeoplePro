a79a5d7e090fadb92be38e4e9654acae81bd00d5
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2023-07-19 16:12:11 +0600

    Backtrack

--- storage/framework/views/48afd21ebcece29a84ba77602a9c7438.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /var/www/html/peoplepro_10/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
